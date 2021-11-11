c5095c056254afa6becd4274230effa05c6da9d6
Zachary Hunter <zachary-hunter-design@gmail.com>
2021-11-10 22:53:47 -0700

    Added filtering

--- characters/sw-characters.js
+++ characters/sw-characters.js
@@ -1,42 +1,56 @@
 import { people } from "../data/people.js";
 
+const mainContent = document.querySelector("#main");
+
+const maleCharacters = people.filter((person) => person.gender === 'male')
+const femaleCharacters = people.filter((person) => person.gender === 'female')
+const otherCharacters = people.filter(person) => {
+    if (person.gender !== 'male' && person.gender !== 'female') {
+        return person
+    }
+}
+
 const header = document.createElement('header')
 const maleButton = document.createElement('button')
 maleButton.textContent = 'Male Characters'
+
+populateDOM(people)
+
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
+
 const femaleButton = document.createElement('button')
 femaleButton.textContent = 'Female Characters'
+
+femaleButton.addEventListener('click', ()=> populateDOM(femaleCharacters))
+
 header.appendChild(maleButton)
 header.appendChild(femaleButton)
 
-const mainContent = document.querySelector("#main");
-
-
 document.body.insertBefore(header, mainContent)
 
-
-const maleCharacters = people.filter(person => {
-    console.log(person.gender)
-    return person.gender === 'male'
-})
-
-const femaleCharacters = people.filter(person => {
-    console.log(person.gender)
-    return person.gender === 'female'
-})
-
-console.log(maleCharacters)
-
-people.forEach((element, index) => {
+function populateDOM(characters) {
+    while(mainContent.firstChild) {
+        mainContent.removeChild(mainContent.firstChild);
+    }
+characters.forEach((element, index) => {
   const charFigure = document.createElement("figure");
   const charImg = document.createElement("img");
-  charImg.src = `https://starwars-visualguide.com/assets/img/characters/${
-    index + 1
-  }.jpg`;
-
+  const charNum = getLastNumber(element.url)
+  charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
   const charCaption = document.createElement("figcaption");
   charCaption.textContent = element.name;
 
   charFigure.appendChild(charImg);
   charFigure.appendChild(charCaption);
   mainContent.appendChild(charFigure);
-});
+})
+}
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    console.log(url.charAt(start))
+    if(url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end) 
+}
