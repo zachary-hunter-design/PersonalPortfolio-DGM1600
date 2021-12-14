6e37503006c57c356d6a741cccf8a54d40b4abea
Zachary Hunter <zachary-hunter-design@gmail.com>
2021-12-13 20:02:50 -0700

    got senators populated

--- congress/congress.js
+++ congress/congress.js
@@ -1,1 +1,32 @@
-import 
+import { senators } from '../data/senators.js'
+
+const senatorDiv = document.querySelector('.senators')
+
+function SimplifiedSenators(senatorArray) {
+    return senatorArray.map(senator => {
+        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+        return {
+        id: senator.id,
+        name: `${senator.first_name}${middleName}${senator.last_name}`,
+        party: senator.party,
+        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`
+        }
+})
+}
+
+populateSenatorDiv(SimplifiedSenators(senators))
+
+function populateSenatorDiv(simpleSenators) {
+    simpleSenators.forEach(senator => {
+        const senFigure = document.createElement('figure')
+        const figImg = document.createElement('img')
+        const figCaption = document.createElement('figcaption')
+
+        figImg.src = senator.imgURL
+        figCaption.textContent = senator.name
+
+        senFigure.appendChild(figImg)
+        senFigure.appendChild(figCaption)
+        senatorDiv.appendChild(senFigure)
+    })
+}
